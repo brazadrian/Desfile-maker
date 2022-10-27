9d54571ecc3e8042d52803fb2b27337e257a3ce1
Pedrinhooo <50207991+Pedrinhooo@users.noreply.github.com>
2022-10-27 16:07:21 -0300

    Estrutura oficial 1.0

--- mysite/static/script.js
+++ mysite/static/script.js
@@ -1,9 +1,23 @@
 let btnMenu = document.querySelector('.toggle-menu');
 let menuHamburguer = document.querySelector('.menu-hamburguer')
+let body = document.querySelector('body')
+let video = document.querySelector('#videos-link')
+let contatos = document.querySelector('#contatos-link')
 
 btnMenu.addEventListener("click", menu);
+
+
+function link(){
+  menuHamburguer.classList.toggle('show')
+  btnMenu.classList.toggle('active')
+  body.classList.toggle('block')
+}
 
 function menu(){
   menuHamburguer.classList.toggle('show')
   btnMenu.classList.toggle('active')
+  body.classList.toggle('block')
 }
+
+video.addEventListener('click', link)
+contatos.addEventListener('click', link)
