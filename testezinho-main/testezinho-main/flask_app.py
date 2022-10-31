fee89b11149e0860dc301e8f22af4c9f28171c72
Pedrinhooo <50207991+Pedrinhooo@users.noreply.github.com>
2022-10-31 15:50:25 -0300

    3.0 Sobre nos

--- testezinho-main/testezinho-main/flask_app.py
+++
@@ -1,9 +0,0 @@
-from flask import Flask, request, render_template
-from flask.wrappers import Response
-import git
-
-app = Flask(__name__)
-
-@app.route('/')
-def principal.html():
-    return render_template("principal.html")
