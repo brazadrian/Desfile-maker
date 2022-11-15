cffb01d75d04371043d61c7c11a4d700b46af5f6
kaigabriel12 <108732958+kaigabriel12@users.noreply.github.com>
2022-11-15 17:22:33 -0300

    Add files via upload

--- mysite/static/curtida.js
+++ mysite/static/curtida.js
@@ -1,5 +1,12 @@
-var img = document.querySelector("#imagem");
-img.setAttribute('src','../static/imgs/coracaovermelho.png');
+const button = document.querySelector('#like');
+const number = document.querySelector('#number');
 
+button.addEventListener('click', () => {
+  let likeValue = document.querySelector('#number').textContent;
+  let newValue = Number(likeValue) + 1;
+  button.classList.add('like');
+  number.innerHTML = newValue;
+});
 
+curtida.addEventListener('click', trocarCoracao)
 
